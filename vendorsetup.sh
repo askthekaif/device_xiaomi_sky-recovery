5acf589e990e8505991214655da8927c85381c64
kAiF <askthekaif@proton.me>
2026-05-11 16:51:13 +0000

    sky: fixed fox variant
    - as sky is based on A12 not A14
    - and our build is stable as always :)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,8 @@ export FOX_VIRTUAL_AB_DEVICE=1
 export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
 export OF_FL_PATH1="/tmp/flashlight"
 export OF_ADVANCED_SECURITY=1
-export FOX_VARIANT="A14"
+export FOX_VARIANT="A12"
+export FOX_BUILD_TYPE="Stable"
 
 # Screen Settings 
 export OF_SCREEN_H=2460
